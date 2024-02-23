4fff680d82199b3379db9a5560f6997939a1ce79
yixing <leiyixing@gmail.com>
2024-02-23 22:24:23 +0000

    first commit

---
+++ src/main/java/com/yokosmile/YokoSmile/YokoSmileApplication.java
@@ -0,0 +1,13 @@
+package com.yokosmile.YokoSmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YokoSmileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YokoSmileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yokosmile/YokoSmile/YokoSmileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yokosmile.YokoSmile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YokoSmileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
